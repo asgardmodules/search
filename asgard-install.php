19b70ec1aad6afac22a0606abdaddc9b0bdc956d
Michel Hognerud <leyou.m@gmail.com>
2014-04-24 07:14:13 +0800

    fixes

--- asgard-install.php
+++ asgard-install.php
@@ -1,4 +1,6 @@
 <?php
-require_once __DIR__.'/../utils/FileManager.php';
+require_once 'paths.php';
+require _CORE_DIR_.'core.php';
+\Asgard\Core\App::loadDefaultApp();
 
-\Asgard\Utils\FileManager::copy(__DIR__.'/app/search', 'app/search');
+\Asgard\Utils\FileManager::copy(__DIR__.'/app/search', _DIR_.'app/search');
